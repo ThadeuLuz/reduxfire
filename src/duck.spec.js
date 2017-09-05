54518ee4eb4d518c345930604b169910a0cf01ac
Thadeu Luz <thadeuluz@gmail.com>
2017-09-05 09:55:04 -0300

    Initial tests

---
+++ src/duck.spec.js
@@ -0,0 +1,14 @@
+import {
+  UPDATE_VALUE,
+  updateValue,
+} from './duck';
+
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+    const expectedAction = {
+      type: UPDATE_VALUE,
+      payload: { bindVar: 'bindVar', snap: 'snap' },
+    };
+    expect(updateValue('bindVar', 'snap')).toEqual(expectedAction);
+  });
+});
