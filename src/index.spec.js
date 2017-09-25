bc2a795bfab9d49f467978c65efa7062a4329bd7
Thadeu Luz <thadeuluz@gmail.com>
2017-09-25 15:17:49 -0300

    First pass for tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
-
-import { setDispatch, bindAsArray, bindAsObject, unbind } from './index';
+import { UPDATE_VALUE } from './duck';
+import { setDispatch, bindAsArray, bindAsObject, unbind, isBound } from './index';
 
 const config = {
   apiKey: 'AIzaSyAUp5E2n1BhqYDeRsSqXvs9X90LVGFz5mY',
@@ -9,17 +9,93 @@ const config = {
 };
 
 firebase.initializeApp(config);
-const ref = firebase
-  .database()
-  .ref('tests')
-  .push();
+const ref = firebase.database().ref('tests');
+
+// Errors
+const setDispatchErr = new Error("ReduxFire: You have to call 'setDispatch(store.dispatch)' before calling 'unbind', 'bindAsArray' or 'bindAsObject'");
+const missingBindVarErr = new Error('ReduxFire: Bind variable must be a string. Got: undefined');
 
 describe('bindAsArray(), bindAsObject() and unbind()', () => {
-  it('should throw when missing bind variable', () => {
-    [bindAsArray, bindAsObject, unbind].forEach(fn => expect(() => fn()).toThrow());
+  it('should throw if called before setDispatch', () => {
+    [bindAsArray, bindAsObject, unbind].forEach(fn => expect(() => fn()).toThrow(setDispatchErr));
   });
 });
 
-setDispatch();
+// setDispatch(() => {});
 
-describe('', () => {});
+describe('setDispatch()', () => {
+  it('should work', () => {
+    expect(() => {
+      setDispatch(() => {});
+    }).not.toThrow();
+  });
+});
+
+describe('bindAsObject()', () => {
+  it('should throw when missing bind Variable', () => {
+    expect(() => {
+      bindAsObject();
+    }).toThrow(missingBindVarErr);
+  });
+
+  it('should dispatch the UPDATE_VALUE action', () =>
+    new Promise((resolve) => {
+      setDispatch((action) => {
+        expect(action.type).toBe(UPDATE_VALUE);
+        expect(action.payload.bindVar).toBe('bindAsObject');
+        expect(action.payload.snap).toBeTruthy();
+        resolve();
+      });
+
+      bindAsObject(ref, 'bindAsObject');
+    }));
+});
+
+describe('bindAsArray()', () => {
+  it('should throw when missing bind Variable', () => {
+    expect(() => {
+      bindAsArray();
+    }).toThrow(missingBindVarErr);
+  });
+
+  it('should dispatch the UPDATE_VALUE action', () =>
+    new Promise((resolve) => {
+      setDispatch((action) => {
+        expect(action.type).toBe(UPDATE_VALUE);
+        expect(action.payload.bindVar).toBe('bindAsArray');
+        expect(action.payload.snap).toBeTruthy();
+        resolve();
+      });
+
+      bindAsObject(ref, 'bindAsArray');
+    }));
+});
+
+describe('unbind()', () => {
+  it('should throw when missing bind Variable', () => {
+    expect(() => {
+      unbind();
+    }).toThrow();
+  });
+  it('should throw if callback is not a function', () => {});
+});
+
+describe('isBound()', () => {
+  it('should throw when missing bind Variable', () => {
+    expect(() => {
+      isBound();
+    }).toThrow();
+  });
+
+  it('should return true for bindAsObject', () => {
+    expect(isBound('bindAsObject')).toBe(true);
+  });
+
+  it('should return true for bindAsArray', () => {
+    expect(isBound('bindAsArray')).toBe(true);
+  });
+
+  it('should return false for unbound bindVar', () => {
+    expect(isBound('unbound')).toBe(false);
+  });
+});
